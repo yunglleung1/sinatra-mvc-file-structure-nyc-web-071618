28c56c8da371b965eb81a4d5010bd0068c09eed2
yunglleung1 <yungl.leung1@gmail.com>
2018-08-06 10:14:50 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < Sinatra::Base
-
+  #This configure block tells controller where to look to find the views & public directory
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
   end
 
+  #This is the home page.
+  #a controller action that can receive/respond to GET request to root URL '/'
+  #loads the index.erb file
   get "/" do
   	erb :index
   end
+
 end
